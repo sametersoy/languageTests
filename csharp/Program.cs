8af00ae50b93cc74641c86cd050e8f4f71454d22
Samet Ersoy <sametersoy@yandex.com>
2023-04-17 18:23:35 +0300

    first commit

---
+++ csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+long startTime = DateTime.Now.Ticks;
+Console.WriteLine(startTime);
+
+
+
+
+long endTime = DateTime.Now.Ticks;
+Console.WriteLine(endTime);
+
+long resultTime = endTime- startTime;
+Console.WriteLine(resultTime);
